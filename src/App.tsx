95538aa1a4412d487e0998ce840a388beadb8764
Mark Britton <mark.britton@gmail.com>
2026-08-25 20:52:03 -0400

    Remove user email from header, move Back button to top-right
    Header top-right now shows Back + Sign out buttons instead of the
    signed-in email; logo stays alone on the left, back to a single line.

--- src/App.tsx
+++ src/App.tsx
@@ -20,10 +20,9 @@ function AppShell() {
       <header className="app-header">
         <div className="app-header-left">
           <img src={logo} alt="LiveStreamFlow" className="app-logo" />
-          <BackButton />
         </div>
         <span className="app-user">
-          {user.email}
+          <BackButton />
           <button type="button" className="link-button" onClick={() => signOut(auth)}>
             Sign out
           </button>
